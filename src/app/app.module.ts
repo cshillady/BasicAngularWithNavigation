20d28d6518a76d5af0c00bb21452eb658a89d0fb
Connor Shillady <c.shillady24@gmail.com>
2022-06-13 16:58:24 -0400

    Hello World App Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page2Component } from './component/page2/page2.component';
+import { Page1Component } from './component/page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page2Component,
+    Page1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+}
